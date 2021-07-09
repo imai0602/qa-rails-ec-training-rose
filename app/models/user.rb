5fd756f12822e691b3df45a53d5aa29dd7a080b1
yuzumotishun <yuzumotishun@gmail.com>
2021-07-10 01:03:40 +0900

    #30 Usersのvalidationを作成しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
+  #郵便番号(ハイフン有り、3桁-4桁)
+  VALID_ZIPCODE_REGEX = /\A\d{3}-\d{4}\z/
+  #メールアドレス
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #電話番号(ハイフン無し) #パスワード 共通で使用(半角数字)
+  VALID_PHONE_AND_PASSWORD_REGEX = /\A[0-9]+\z/
+
+  validates :password, presence: true, length: {minimum: 6, maximum: 15}, format: { with: VALID_PHONE_AND_PASSWORD_REGEX }
+  validates :last_name, presence: true, length: {maximum: 10}
+  validates :first_name, presence: true, length: {maximum: 10}
+  validates :zipcode, presence: true, format: { with: VALID_ZIPCODE_REGEX }
+  validates :prefecture, presence: true, length: {maximum: 5}
+  validates :municipality, presence: true, length: {maximum: 10}
+  validates :address, presence: true, length: {maximum: 15}
+  validates :apartments, presence: true, length: {maximum: 20}
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :phone_number, presence: true, length: { maximum: 15 }, format: { with: VALID_PHONE_AND_PASSWORD_REGEX }
+
   belongs_to :user_classification
 end
