98e24a2b3a241b22909e09c57e4f6ddf2363fe38
imai hirohito <h.imai.c.9305@gmail.com>
2021-07-11 07:35:22 +0900

    #29 データ投入 Seed Users関連

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,29 @@
+
+user_classification = UserClassification.create!(
+  user_classification_name: "test_id"
+)
+
+2.times do |n|
+  user = User.create!(
+    password:"pass",
+    last_name:"太郎#{n + 1}",
+    first_name:"探求#{n + 1}",
+    zipcode:"000-0000",
+    prefecture:"東京都",
+    municipality:"探求区",
+    address:"0-0",
+    apartments:"探求学園10#{n + 1}",
+    email:"e#{n + 1}@e.com",
+    phone_number:"000-0000-000#{n + 1}",
+    company_name:"探求学園",
+    delete_flag: false,
+    user_classification_id: user_classification.id
+  )
+end
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
