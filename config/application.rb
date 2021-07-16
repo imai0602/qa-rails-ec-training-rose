68423e1eb5baa07357ae36d0cbccd91af1d7ad68
imai hirohito <h.imai.c.9305@gmail.com>
2021-07-17 00:47:52 +0900

    #24 ユーザー情報画面作成

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,15 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
+    config.generators do |g|
+      config.load_defaults 6.0
+      g.helper false
+      g.test_framework false
+      g.assets false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    end
   end
 end
