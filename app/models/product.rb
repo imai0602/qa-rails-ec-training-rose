061766fd685605325eeb3c8a2a5509f1e5ef9c4a
yuzumotishun <yuzumotishun@gmail.com>
2021-07-19 21:46:00 +0900

    #27 ER図を確認し、不足しているリレーションを追記しました。

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :purchases, dependent: :destroy
+  has_many :order_details, dependent: :destroy
   belongs_to :category
   belongs_to :sale_status
   belongs_to :product_status
