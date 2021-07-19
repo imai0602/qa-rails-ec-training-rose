2dc53c9bec49ed08f1e72c635e46f32ec5a69bdb
Yuki.tachi <y.tachi0219@gmail.com>
2021-07-19 23:11:01 +0900

    rubocop指摘修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,40 @@
-
 user_classification = UserClassification.create!(
-  user_classification_name: "一般ユーザー"
+  user_classification_name: "一般ユーザー",
 )
 
 2.times do |n|
-  user = User.create!(
-    password:"testpass",
-    last_name:"太郎#{n + 1}",
-    first_name:"探求#{n + 1}",
-    zipcode:"000-0000",
-    prefecture:"東京都",
-    municipality:"探求区",
-    address:"0-0",
-    apartments:"探求学園10#{n + 1}",
-    email:"e#{n + 1}@example.com",
-    phone_number:"0000000000#{n + 1}",
-    company_name:"探求学園",
+  User.create!(
+    password: "testpass",
+    last_name: "太郎#{n + 1}",
+    first_name: "探求#{n + 1}",
+    zipcode: "000-0000",
+    prefecture: "東京都",
+    municipality: "探求区",
+    address: "0-0",
+    apartments: "探求学園10#{n + 1}",
+    email: "e#{n + 1}@example.com",
+    phone_number: "0000000000#{n + 1}",
+    company_name: "探求学園",
     delete_flag: false,
-    user_classification_id: user_classification.id
+    user_classification_id: user_classification.id,
   )
 end
 
-["販売中","欠品中"].each do |sale_status|
+["販売中", "欠品中"].each do |sale_status|
   SaleStatus.create!(
-    sale_status_name: sale_status
+    sale_status_name: sale_status,
   )
 end
 
-["良","傷あり"].each do |product_status|
+["良", "傷あり"].each do |product_status|
   ProductStatus.create!(
-    product_status_name: product_status
+    product_status_name: product_status,
   )
 end
 
-["食料品","日用品"].each do |category|
+["食料品", "日用品"].each do |category|
   Category.create!(
-    category_name: category
+    category_name: category,
   )
 end
 
@@ -48,7 +47,7 @@ Product.create!(
   product_status_id: 1,
   regist_date: "2021/7/10",
   user_id: 1,
-  delete_flag: false
+  delete_flag: false,
 )
 
 Product.create!(
@@ -60,7 +59,7 @@ Product.create!(
   product_status_id: 2,
   regist_date: "2021/7/20",
   user_id: 1,
-  delete_flag: false
+  delete_flag: false,
 )
 
 Product.create!(
@@ -72,7 +71,7 @@ Product.create!(
   product_status_id: 1,
   regist_date: "2021/7/11",
   user_id: 2,
-  delete_flag: false
+  delete_flag: false,
 )
 
 Product.create!(
@@ -84,25 +83,25 @@ Product.create!(
   product_status_id: 2,
   regist_date: "2021/7/22",
   user_id: 2,
-  delete_flag: false
+  delete_flag: false,
 )
 
-["準備中","発送中"].each do |shipment_status|
+["準備中", "発送中"].each do |shipment_status|
   ShipmentStatus.create!(
-    shipment_status_name: shipment_status
+    shipment_status_name: shipment_status,
   )
 end
 
 Order.create!(
   user_id: 1,
   order_date: Time.zone.parse("2021-07-17 15:30:59"),
-  order_number: "1313131313"
+  order_number: "1313131313",
 )
 
 Order.create!(
   user_id: 2,
   order_date: Time.zone.parse("2021-07-17 15:35:59"),
-  order_number: "2424242424"
+  order_number: "2424242424",
 )
 
 OrderDetail.create!(
