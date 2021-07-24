30e209c9e28a3ecc5f4746ac24b768f1b4853fee
ayumi kamada <dmrdmr58@gmail.com>
2021-07-24 20:17:11 +0900

    #11 商品検索画面(検索ロジック以外)実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,9 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    @products = Product.page(params[:page]).per(15)
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
   end
