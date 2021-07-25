9908df14b1531d8d821dc83f3e25866608deb671
ayumi kamada <dmrdmr58@gmail.com>
2021-07-25 13:01:13 +0900

    #11 indexを１行に纏めて記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
-    @products = Product.page(params[:page]).per(15)
+    @products = Product.all.page(params[:page]).per(15)
   end
 
   def show
