e1103a41fee902ce06c95fb34993ed7398a44194
yuzumotishun <yuzumotishun@gmail.com>
2021-07-29 18:45:06 +0900

    #18 helperにあったメソッドをモデルに移しました。

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,17 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
+
+
+  def shipment_prepared?(order)
+    order.order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == "準備中" }
+  end
+
+  def total_price(order)
+    total_price = 0
+    order.order_details.each do |order_detail|
+      total_price += order_detail.sub_total_price
+    end
+    total_price
+  end
 end
