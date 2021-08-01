0bdbfebfa08d30ac504435cfdd14dbd3927e9043
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-01 22:46:48 +0900

    #17 ユーザー情報更新（最新版のdevelopmentブランチの最新コードを取り込む為のコミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      flash[:notice] = "ユーザー情報を更新できませんでした。"
+      render "edit"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation, :last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments,
+                                   :company_name, :email, :phone_number)
+    end
 end
