2c0c8139d7b3af76e5d7f63c7eddd5dd2070b9b1
yuzumotishun <yuzumotishun@gmail.com>
2021-08-02 17:28:17 +0900

    #6 商品検索のロジックを実装しました。また、カテゴリーによる絞り込みのロジックも実装しました。

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,11 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :sale_status
   belongs_to :product_status
+
+  def self.search(search, category)
+    products = Product.all
+    products = products.where(["product_name LIKE ?", "%#{search}%"]) if search.present?
+    products = products.where(category_id: category) if category.present?
+    products
+  end
 end
