1617789756a0233a427a2b14828147f513f618f7
ayumi kamada <dmrdmr58@gmail.com>
2021-08-02 22:30:21 +0900

    #14 rubocop実行

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
-
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
+
   # 現在ログインしているユーザーを返す (ユーザーがログイン中の場合のみ)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -15,6 +15,6 @@ module SessionsHelper
 
   def log_out
     session.delete(:user_id)
-    @current_user = nil
+    @current_user = nil # rubocop:disable Rails/HelperInstanceVariable
   end
 end
