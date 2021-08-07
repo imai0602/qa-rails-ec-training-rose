49c8466de7d32d0372ead0db685c217b2f4c144c
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-07 14:39:25 +0900

    ログイン　ログアウト機能フラッシュメッセージ　rubocop警告対応

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      flash[:success] = 'ログインしました'
+      flash[:success] = "ログインしました"
       redirect_to current_user
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash.now[:danger] = "ログインに失敗しました"
       render "new"
     end
   end
 
   def destroy
     log_out if logged_in?
-    flash[:notice] = 'ログアウトしました'
+    flash[:notice] = "ログアウトしました"
     redirect_to login_path
   end
 end
