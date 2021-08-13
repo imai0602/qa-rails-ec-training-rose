c96ecca19ea797b8e4cf80941899a16bfc665978
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-13 20:38:48 +0900

    #5 注文履歴

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,11 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
   end
+
+  def index
+    @orders = current_user.orders.page(params[:page]).per(15).order(id: :DESC)
+    page = (params[:page] || 1).to_i
+    par_page = 15
+    @page_adj = (page - 1) * par_page + 1 # ページネーション機能の２ページ目以降のNo.調整
+  end
 end
