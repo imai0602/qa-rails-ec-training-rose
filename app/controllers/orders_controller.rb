53095552291fe8b1fc4584a3c7d63829970851db
ayumi kamada <dmrdmr58@gmail.com>
2021-08-13 21:15:02 +0900

    #13 {}削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :correct_user, { only: [:show] }
+  before_action :correct_user, only: :show
 
   def show
     @order = Order.find_by(id: params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, { only: [:show, :edit] }
-  before_action :correct_user, { only: [:show, :edit] }
+  before_action :logged_in_user, only: [:show, :edit]
+  before_action :correct_user, only: [:show, :edit]
 
   def show
     @user = User.find_by(id: params[:id])
