8634e7a35738210ba477762b85d8c004461d3bb7
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-14 00:22:57 +0900

    #5 注文履歴 注文日時の降順に修正& 不要なページネーションの調整機能を削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,9 +4,6 @@ class OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_user.orders.page(params[:page]).per(15).order(id: :DESC)
-    page = (params[:page] || 1).to_i
-    par_page = 15
-    @page_adj = (page - 1) * par_page + 1 # ページネーション機能の２ページ目以降のNo.調整
+    @orders = current_user.orders.page(params[:page]).per(15).order(order_date: :DESC)
   end
 end
