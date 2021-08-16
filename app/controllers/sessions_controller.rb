6e5442144dbc3ae2476f7d766342f479d83c9210
yuzumotishun <yuzumotishun@gmail.com>
2021-08-16 23:50:12 +0900

    #1 ゲストを作成し、ゲストでログインするメソッドを作成しました。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,31 @@ class SessionsController < ApplicationController
     flash[:notice] = "ログアウトしました"
     redirect_to login_path
   end
+
+  def guest_login
+    guest_person = guest_create
+    login = guest_person
+    flash[:success] = "ゲストユーザーとしてログインしました。"
+    redirect_to products_path
+    login
+  end
+
+  def guest_create
+    User.find_or_create_by!(email: "guest@example.com") do |user|
+      user.id = 500
+      user.password = "testpass"
+      user.last_name = "ゲスト"
+      user.first_name = "ユーザ"
+      user.zipcode = "000-0000"
+      user.prefecture = "東京都"
+      user.municipality = "探求区"
+      user.address = "0-0"
+      user.apartments = "探求学園10"
+      user.email = "guest@example.com"
+      user.phone_number = "00000000000"
+      user.company_name = "探求学園"
+      user.delete_flag = false
+      user.user_classification_id = 1
+    end
+  end
 end
