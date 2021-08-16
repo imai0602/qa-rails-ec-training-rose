abb8bcdd91a59cc46a99bf65586856aec45312b9
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-16 23:53:31 +0900

    #9 ユーザー情報登録ページ、登録処理 誤った修正内容を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:password, :password_confirmation, :last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments,
-                                   :email, :phone_number, :company_name)
+                                   :email, :phone_number)
     end
 end
