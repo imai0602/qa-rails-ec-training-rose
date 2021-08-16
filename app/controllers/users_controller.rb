02624a66417849f2089b8ad36e7e05518c58b1a2
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-16 23:28:01 +0900

    #9 ユーザー情報登録ページ、登録処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,22 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[show edit]
   before_action :correct_user, only: %i[show edit]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.user_classification_id = 1 # ユーザー種別を購入者に限定する
+    if @user.save
+      flash[:success] = "ユーザーを登録しました。こちらからログインしてください。"
+      redirect_to login_path
+    else
+      flash[:danger] = "ユーザー情報を登録できませんでした。"
+      render "new"
+    end
+  end
+
   def show
     @user = User.find_by(id: params[:id])
   end
@@ -25,6 +41,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:password, :password_confirmation, :last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments,
-                                   :email, :phone_number)
+                                   :email, :phone_number, :company_name)
     end
 end
