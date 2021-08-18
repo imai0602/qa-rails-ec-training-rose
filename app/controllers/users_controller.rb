75a34e92d965cbf37b99a82640140eba3eb4411c
yuzumotishun <yuzumotishun@gmail.com>
2021-08-19 00:02:36 +0900

    #1 updateアクション内で条件分岐していたのをbefore_actionに変更しました。また、「ユーザー削除」のissueで取り組むTODOを追記しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[show edit]
   before_action :correct_user, only: %i[show edit]
-
+  ##### TODO: 「ユーザー退会」のissueで "only: %i[update delete]"とする。 ######
+  before_action :ensure_normal_user, only: :update
+  #########################################################################
   def show
     @user = User.find_by(id: params[:id])
   end
@@ -12,13 +14,19 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    # ゲストユーザの場合、更新ができないように設定
-    if @user.update(user_params) && @user.email != "guest@example.com"
+    if @user.update(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
       flash[:danger] = "ユーザー情報を更新できませんでした。"
       render "edit"
+    end
+  end
+
+  def ensure_normal_user
+    if current_user.email == "guest@example.com"
+      flash[:danger] = "ゲストユーザーは更新・退会はできません。"
+      redirect_to edit_user_path
     end
   end
 
