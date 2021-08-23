d94a79c16e76ce18ac97ef01a1d17b01c8543f70
ayumi kamada <dmrdmr58@gmail.com>
2021-08-23 22:52:29 +0900

    #3 注文番号生成時のロジックを修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,19 +14,19 @@ class OrdersController < ApplicationController
       return redirect_to carts_show_path
     end
 
-    cart = session[:cart]
+    #cart = session[:cart]
 
     order = current_user.orders.create!(
       order_date: Time.current,
-      order_number: "#{cart.object_id % 100}#{complex_data}",
+      order_number: generate_order_number,
     )
 
-    session[:cart].each do |c|
+    session[:cart].each do |cart|
       order.order_details.create(
-        product_id: c["product_id"],
+        product_id: cart["product_id"],
         shipment_status_id: 1,
-        order_detail_number: "#{c["product_id"]}#{complex_data}".rjust(16, "0"),
-        order_quantity: c["quantity"],
+        order_detail_number: "1111111111111111", #使用する用途が無い為仮の番号
+        order_quantity: cart["quantity"],
       )
     end
 
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
 
   private
 
-    def complex_data
-      Time.current.strftime("%y%m%d%H%M").to_i
+    def generate_order_number
+      "#{current_user.id}#{Time.current.strftime("%y%m%d%H%M").to_i}".rjust(16, "0")
     end
 end
