c549043f2d3c9e077a00ee61af1f8eec6e29d1a4
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-23 20:55:08 +0900

    #4 注文キャンセル 準備中の注文商品の削除機能を実装。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,14 @@ class OrdersController < ApplicationController
   def index
     @orders = current_user.orders.page(params[:page]).per(15).order(order_date: :DESC)
   end
+
+  def destroy
+    order = current_user.orders.find_by(id: params[:id])
+    order.order_details.destroy_by(shipment_status_id: 1) # 準備中のorder_detailのみを削除
+    if order.order_details.count == 0 # order_detailが０個の場合は、order自体を削除する
+      order.destroy! # order自体を削除
+    end
+    flash[:success] = "準備中の注文をキャンセルしました。"
+    redirect_to orders_path
+  end
 end
