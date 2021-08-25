dbc80991a23ff28d8acb88702c4d8432aa9e7eff
imai hirohito <h.imai.c.9305@gmail.com>
2021-08-25 20:32:37 +0900

    注文キャンセル order_detailが０個の場合の記述をorder.order_details.blank?に修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,7 @@ class OrdersController < ApplicationController
   def destroy
     order = current_user.orders.find_by(id: params[:id])
     order.order_details.destroy_by(shipment_status_id: 1) # 準備中のorder_detailのみを削除
-    if order.order_details.count == 0 # order_detailが０個の場合は、order自体を削除する
+    if order.order_details.blank? # order_detailが０個の場合は、order自体を削除する
       order.destroy! # order自体を削除
     end
     flash[:success] = "準備中の注文をキャンセルしました。"
